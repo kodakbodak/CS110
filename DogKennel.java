069c2dbe8283136b0f078d91689899e9dcc01f91
phosphor-1 <isaacpaintball@gmail.com>
2023-06-01 23:01:15 -0700

    .

---
+++ DogKennel.java
@@ -0,0 +1,23 @@
+// Author: Isaac Smith
+// File: Lab 9 Dog Kennel using Arrays
+// Date: 05/25/2023
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class DogKennel {
+
+
+    public static void main (String [] args) {
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        Dog dog3 = new Dog();
+
+
+        
+
+    }
+
+
+}
